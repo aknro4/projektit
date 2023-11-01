d4a5ba6bd9b7bcb116bd7a61803389971477a98d
Aaro Kolu <aarokolu4@gmail.com>
2023-11-01 15:13:06 +0200

    Section 3 Done

--- courses/machine_learning/section_3/data_preprocessing_tools.py
+++ courses/machine_learning/section_3/data_preprocessing_tools.py
@@ -5,6 +5,8 @@ from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 data_csv = "Data.csv"
 dataset = pd.read_csv(data_csv)
@@ -43,7 +45,7 @@ imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 print("Handling missing data \n", X, "\n")
 
-# Encoding categorical data Example: Spain, France, Germany
+# Encoding categorical data or "Dummy data"  Example: Spain, France, Germany
 # Spain = 0 0 1, France = 0 1 0, Germany = 1 0 0
 # one hot encoding
 ct = ColumnTransformer(transformers=[("encoder", OneHotEncoder(), [0])], remainder="passthrough")
@@ -54,3 +56,22 @@ print("Encoding categorical data \n", X, "\n")
 le = LabelEncoder()
 y = le.fit_transform(y)
 print("Encoding dependent variable \n", y, "\n")
+
+# Splitting dataset to Training set and Test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+print("X train set \n", X_train, "\n X test set \n", X_test)
+print("y train set \n", y_train, "\n y test set \n", y_test)
+
+# Feature scaling should be done after Splitting the data
+# To avoid information leakage ->
+# Test set should be something new which we evaluate model on new observations.
+
+# Feature scaling
+scaler = StandardScaler()
+
+# Do not use encoded "Dummy" values/data on Feature Scaling! It will ruin the data
+X_train[:, 3:] = scaler.fit_transform(X_train[:, 3:])
+X_test[:, 3:] = scaler.transform(X_test[:, 3:])
+
+print("X train scaled \n", X_train, "\n X test scaled\n", X_test)
