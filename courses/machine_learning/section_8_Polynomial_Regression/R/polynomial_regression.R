9220a4a989100afcf99861f7bb816ade0fca0767
Aaro Kolu <aarokolu4@gmail.com>
2023-11-04 07:01:52 +0200

    Section 8,R done

--- courses/machine_learning/section_8_Polynomial_Regression/R/polynomial_regression.R
+++ courses/machine_learning/section_8_Polynomial_Regression/R/polynomial_regression.R
@@ -1,15 +1,44 @@
 dataset = read.csv("Position_Salaries.csv")
-dataset = dataset[,2:3]
+dataset = dataset[2:3]
 
-# Splitting data into test set and training set
-#install.packages("caTools")
-library(caTools)
-set.seed(123)
-split = sample.split(dataset$Salary, SplitRatio = 0.8)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
-
-lin_reg = lm(forumla = Salary ~ Level,
+# Fitting linear regression to the dataset
+# This no work sadge
+lin_reg = lm(formula = Salary ~ Level,
              data = dataset)
 
+# Fitting polynomial regression to the dataset
+dataset$Level2 = dataset$Level^2
+dataset$Level3 = dataset$Level^3
+dataset$Level4 = dataset$Level^4
+poly_reg = lm(formula = Salary ~ .,
+              data = dataset)
 
+library(ggplot2)
+# Visualising Linear regression
+ggplot()+
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = "red") +
+  geom_line(aes(x = dataset$Level, y = predict(lin_reg, newdata = dataset)),
+                colour = "blue") +
+  ggtitle("Linear Regression") +
+  xlab("Level") +
+  ylab("Salary")
+
+# Visualising Polynomial regression
+ggplot()+
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = "red")+
+  geom_line(aes(x = dataset$Level, y = predict(poly_reg,newdata = dataset)),
+                colour = "blue") +
+  ggtitle("Polynomial Regression") +
+  xlab("Level") +
+  ylab("Salary")
+
+# Predicting new result whit linear regression
+predict(lin_reg, newdata = data.frame(Level = 6.5))
+
+# Predicting new result whit Polynomial regression
+predict(poly_reg, newdata = data.frame(Level = 6.5,
+                                       Level2 = 6.5^2,
+                                       Level3 = 6.5^3,
+                                       Level4 = 6.5^4))
