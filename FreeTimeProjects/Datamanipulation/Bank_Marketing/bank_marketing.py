c57ec6ff5625749124b0a6440a88d59045675fba
Aaro Kolu <aarokolu4@gmail.com>
2023-11-17 08:16:53 +0200

    Bank marketing start

---
+++ FreeTimeProjects/Datamanipulation/Bank_Marketing/bank_marketing.py
@@ -0,0 +1,11 @@
+from ucimlrepo import fetch_ucirepo
+
+# fetch dataset
+bank_marketing = fetch_ucirepo(id=222)
+
+# data (as pandas dataframes)
+X = bank_marketing.data.features
+y = bank_marketing.data.targets
+
+
+# https://archive.ics.uci.edu/dataset/222/bank+marketing
