a22380dfeaa395b87444ca28cb06bd518b48300d
Aaro Kolu <aarokolu4@gmail.com>
2023-11-25 19:34:35 +0200

    Getting values from excel... Issues with returning values back to excel

--- FreeTimeProjects/Datamanipulation/Bank_Marketing/predictions.py
+++ FreeTimeProjects/Datamanipulation/Bank_Marketing/predictions.py
@@ -1,10 +1,18 @@
 import pandas
+import xlwings as xw
 
-df = pandas.read_csv("training_data/whole_dataset.csv")
-unique_df = df.drop_duplicates()
+# df = pandas.read_csv("training_data/whole_dataset.csv")
+# unique_df = df.drop_duplicates()
 
-to_CSV = unique_df
-to_CSV.to_csv("training_data/unique_data.csv", index=False)
+# to_CSV = unique_df
+# to_CSV.to_csv("training_data/unique_data.csv", index=False)
 
 
-print(unique_df)
+def get_data_from_excel():
+    # Get data from excel
+    ws = xw.Book("Excel/bank_marketing.xlsx").sheets['Dashboard']
+
+    # Selecting data from the Excel
+    v1 = ws.range("A44:M44").value
+
+    return v1
